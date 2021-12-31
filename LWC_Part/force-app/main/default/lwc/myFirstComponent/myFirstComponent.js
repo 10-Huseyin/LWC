a8a538d93c04e5babe5c5c16d20cfc3f7dfcc361
huseyin <ukwebsoftware@gmail.com>
2021-12-30 20:58:08 -0500

    Hello-World( JStoHTML )

---
+++ LWC_Part/force-app/main/default/lwc/myFirstComponent/myFirstComponent.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class MyFirstComponent 
+extends LightningElement {
+
+userName='Mary';
+
+myContact={"firstName":"Aliya",
+         "lastName":"Kuanwa"   
+         };
+
+}
